d873ed6e1a3b25e8178484d04499a0d9d139d92c
sixthsurge <surgecrafter@gmail.com>
2025-12-29 23:10:40 +0000

    can't spell progress without poggers

--- chess-lib/src/movegen.rs
+++ chess-lib/src/movegen.rs
@@ -1,6 +1,16 @@
+/*
+ * Terminology:
+ *  - Attack set: bitboard of squares attacked by a piece on a given square.
+ *  - Pseudolegal: move thats are possible if king safety is ignored.
+ */
+
+mod precomputed_bitboards;
 mod sliding;
 
-use crate::board::{Board, Move};
+use crate::{
+    board::{Bitboard, Move, Square},
+    movegen::sliding::SlidingAttackTable,
+};
 
 /// The maximum number of legal moves in a reachable chess position.
 pub const MAXIMUM_LEGAL_MOVES: usize = 218;
@@ -9,19 +19,17 @@ pub const MAXIMUM_LEGAL_MOVES: usize = 218;
 #[derive(Clone, Debug)]
 pub struct MoveGenerator {
     legal_moves: Vec<Move>,
+    bishop_attack_table: SlidingAttackTable,
+    rook_attack_table: SlidingAttackTable,
 }
 
 impl MoveGenerator {
     pub fn new() -> Self {
         Self {
             legal_moves: Vec::with_capacity(MAXIMUM_LEGAL_MOVES),
+            bishop_attack_table: SlidingAttackTable::compute_for_bishop(),
+            rook_attack_table: SlidingAttackTable::compute_for_rook(),
         }
-    }
-
-    /// Calculate and return the list of legal moves in the chess position.
-    /// Should try to avoid allocating.
-    pub fn get_legal_moves<'s>(&'s mut self, board: &Board) -> &'s [Move] {
-        &self.legal_moves
     }
 }
 
@@ -29,4 +37,34 @@ impl Default for MoveGenerator {
     fn default() -> Self {
         Self::new()
     }
+}
+
+/// Returns the bitboard of squares attacked by a white pawn on the given square.
+pub fn white_pawn_attacks(sq: Square) -> Bitboard {
+    Bitboard(precomputed_bitboards::WHITE_PAWN_ATTACK_SETS[sq.as_u8() as usize])
+}
+
+/// Returns the bitboard of squares attacked by a black pawn on the given square.
+pub fn black_pawn_attacks(sq: Square) -> Bitboard {
+    Bitboard(precomputed_bitboards::BLACK_PAWN_ATTACK_SETS[sq.as_u8() as usize])
+}
+
+/// Returns the bitboard of squares attacked by a knight on the given square.
+pub fn knight_attacks(sq: Square) -> Bitboard {
+    Bitboard(precomputed_bitboards::KNIGHT_ATTACK_SETS[sq.as_u8() as usize])
+}
+
+/// Returns the bitboard of squares attacked by a king on the given square.
+pub fn king_attacks(sq: Square) -> Bitboard {
+    Bitboard(precomputed_bitboards::KING_ATTACK_SETS[sq.as_u8() as usize])
+}
+
+/// Returns the bitboard of squares attacked by an unobstructed rook on the given square.
+pub fn unobstructed_rook_attacks(sq: Square) -> Bitboard {
+    Bitboard(precomputed_bitboards::UNOBSTRUCTED_ROOK_ATTACK_SETS[sq.as_u8() as usize])
+}
+
+/// Returns the bitboard of squares attacked by an unobstructed bishop on the given square.
+pub fn unobstructed_bishop_attacks(sq: Square) -> Bitboard {
+    Bitboard(precomputed_bitboards::UNOBSTRUCTED_BISHOP_ATTACK_SETS[sq.as_u8() as usize])
 }
