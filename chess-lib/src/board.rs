666c4c8b526fdeedd1edc52a6261d7ca2161ed7c
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-12-30 17:32:34 +0000

    Working move apply / undo with tests

--- chess-lib/src/board.rs
+++ chess-lib/src/board.rs
@@ -241,17 +241,173 @@ impl Board {
         output
     }
 
-    pub fn make_move(&mut self, mv: Move) -> Result<(), String> {
-        todo!();
+    fn castling_rights_of(&mut self, color: Color) -> &mut CastlingRights {
+        if color == Color::White {
+            &mut self.white_castling_rights
+        } else {
+            &mut self.black_castling_rights
+        }
     }
 
-    pub fn make_null_move(&mut self) -> UnmakeInfo {
+    pub fn make_move(&mut self, mv: Move) -> UnmakeInfo {
+        let source_piece = self.pieces.get(mv.source).unwrap();
+        let destination_piece = self.pieces.get(mv.destination).map(|p| p.kind());
+        let dx = mv.destination.file().as_u8() as i32 - mv.source.file().as_u8() as i32;
+        let dy = mv.destination.rank().as_u8() as i32 - mv.source.rank().as_u8() as i32;
+
+        let mut unmake_info = UnmakeInfo {
+            piece: source_piece.kind(),
+            source: mv.source,
+            destination: mv.destination,
+            captured: destination_piece, // Changed for en passant
+            old_en_passant_destination: self.en_passant_destination,
+            old_castling_rights: *self.castling_rights_of(self.color_to_move),
+            old_halfmoves_since_event: self.halfmoves_since_event,
+        };
+
+        self.en_passant_destination = None;
+
+        match (
+            source_piece.kind(),
+            destination_piece,
+            (dx, dy),
+            mv.promotion,
+        ) {
+            (PieceKind::Pawn, None, (dx, _), _) if dx != 0 => {
+                // En passant
+                self.pieces.set(mv.source, None);
+                self.pieces.set(mv.destination, Some(source_piece));
+                self.pieces
+                    .set(Square::at(mv.destination.file(), mv.source.rank()), None);
+                unmake_info.captured = Some(PieceKind::Pawn);
+            }
+            (_, _, (_, _), Some(piece)) => {
+                // Promotion
+                self.pieces.set(mv.source, None);
+                self.pieces.set(
+                    mv.destination,
+                    Some(Piece::new(piece, source_piece.color())),
+                );
+            }
+            (PieceKind::King, _, (-2, _), _) => {
+                // Long castle
+                *self.castling_rights_of(source_piece.color()) = CastlingRights::none();
+                self.pieces.set(mv.source, None);
+                self.pieces.set(mv.destination, Some(source_piece));
+                self.pieces
+                    .set(Square::at(BoardFile::A, mv.source.rank()), None);
+                self.pieces.set(
+                    Square::at(BoardFile::D, mv.source.rank()),
+                    Some(Piece::new(PieceKind::Rook, source_piece.color())),
+                );
+            }
+            (PieceKind::King, _, (2, _), _) => {
+                // Short
+                *self.castling_rights_of(source_piece.color()) = CastlingRights::none();
+                self.castling_rights_of(source_piece.color()).queenside = false;
+                self.castling_rights_of(source_piece.color()).kingside = false;
+                self.pieces.set(mv.source, None);
+                self.pieces.set(mv.destination, Some(source_piece));
+                self.pieces
+                    .set(Square::at(BoardFile::H, mv.source.rank()), None);
+                self.pieces.set(
+                    Square::at(BoardFile::F, mv.source.rank()),
+                    Some(Piece::new(PieceKind::Rook, source_piece.color())),
+                );
+            }
+            (PieceKind::Pawn, _, (_, 2 | -2), _) => {
+                debug_assert!(((mv.destination.rank().as_u8() as i32 - (dy / 2)) as u8) < 8);
+                // SAFETY: if dy.abs() == 2, then there is a rank between the source and destination rank.
+                //     Subtracting dy / 2 from the destination rank gets that rank.
+                unsafe {
+                    self.en_passant_destination = Some(Square::at(
+                        mv.destination.file(),
+                        BoardRank::from_u8_unchecked(
+                            (mv.destination.rank().as_u8() as i32 - (dy / 2)) as u8,
+                        ),
+                    ));
+                }
+
+                self.pieces.set(mv.source, None);
+                self.pieces.set(mv.destination, Some(source_piece));
+            }
+            _ => {
+                self.pieces.set(mv.source, None);
+                self.pieces.set(mv.destination, Some(source_piece));
+            }
+        };
+
+        if unmake_info.captured.is_some() {
+            self.halfmoves_since_event = 0;
+        }
+
         self.color_to_move = !self.color_to_move;
-        UnmakeInfo::NullMove
+        if self.color_to_move == Color::White {
+            self.fullmoves += 1;
+        }
+
+        unmake_info
     }
 
-    pub fn unmake_last_move(&mut self) {
-        todo!();
+    pub fn unmake_last_move(&mut self, um: UnmakeInfo) {
+        let dx = um.destination.file().as_u8() as i32 - um.source.file().as_u8() as i32;
+
+        self.color_to_move = !self.color_to_move;
+        self.halfmoves_since_event = um.old_halfmoves_since_event;
+        if self.color_to_move == Color::Black {
+            self.fullmoves -= 1;
+        }
+        self.en_passant_destination = um.old_en_passant_destination;
+        *self.castling_rights_of(self.color_to_move) = um.old_castling_rights;
+
+        // Don't need to handle promotion or leap
+        match (um.piece, um.destination.rank(), dx) {
+            (PieceKind::Pawn, _, _) if Some(um.destination) == um.old_en_passant_destination => {
+                // En Passant
+                self.pieces.set(um.destination, None);
+                self.pieces
+                    .set(um.source, Some(Piece::new(um.piece, self.color_to_move)));
+
+                self.pieces.set(
+                    Square::at(um.destination.file(), um.source.rank()),
+                    Some(Piece::new(PieceKind::Pawn, !self.color_to_move)),
+                );
+            }
+            (PieceKind::King, _, -2) => {
+                // Long castle
+                self.pieces.set(um.destination, None);
+                self.pieces
+                    .set(um.source, Some(Piece::new(um.piece, self.color_to_move)));
+
+                self.pieces
+                    .set(Square::at(BoardFile::D, um.source.rank()), None);
+                self.pieces.set(
+                    Square::at(BoardFile::A, um.source.rank()),
+                    Some(Piece::new(PieceKind::Rook, self.color_to_move)),
+                );
+            }
+            (PieceKind::King, _, 2) => {
+                // Short castle
+                self.pieces.set(um.destination, None);
+                self.pieces
+                    .set(um.source, Some(Piece::new(um.piece, self.color_to_move)));
+
+                self.pieces
+                    .set(Square::at(BoardFile::F, um.source.rank()), None);
+                self.pieces.set(
+                    Square::at(BoardFile::H, um.source.rank()),
+                    Some(Piece::new(PieceKind::Rook, self.color_to_move)),
+                );
+            }
+            _ => {
+                self.pieces.set(
+                    um.destination,
+                    um.captured.map(|k| Piece::new(k, !self.color_to_move)),
+                );
+                self.pieces
+                    .set(um.source, Some(Piece::new(um.piece, self.color_to_move)));
+            }
+        };
     }
 }
 
@@ -259,6 +415,15 @@ impl Board {
 pub struct CastlingRights {
     kingside: bool,
     queenside: bool,
+}
+
+impl CastlingRights {
+    pub fn none() -> CastlingRights {
+        CastlingRights {
+            kingside: false,
+            queenside: false,
+        }
+    }
 }
 
 impl Default for CastlingRights {
@@ -272,17 +437,15 @@ impl Default for CastlingRights {
 
 /// Information necessary to unmake the last move
 #[derive(Clone, Copy, Debug)]
-pub enum UnmakeInfo {
-    Move {
-        /// original piece kind, in case of promotion
-        piece: PieceKind,
-        source: Square,
-        destination: Square,
-        captured: Option<PieceKind>,
-        old_castling_rights: CastlingRights,
-        old_en_passant_destination: Option<Square>,
-    },
-    NullMove,
+pub struct UnmakeInfo {
+    /// original piece kind, in case of promotion
+    pub piece: PieceKind,
+    pub source: Square,
+    pub destination: Square,
+    pub captured: Option<PieceKind>,
+    pub old_en_passant_destination: Option<Square>,
+    pub old_castling_rights: CastlingRights,
+    pub old_halfmoves_since_event: usize,
 }
 
 #[cfg(test)]
@@ -293,5 +456,134 @@ mod tests {
     fn test_fen_forward_back() {
         const FEN: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
         assert_eq!(Board::from_fen(FEN).unwrap().to_fen(), FEN);
+    }
+
+    fn test_move_and_unmake(fen_before: &str, mv: Move, fen_after: &str) {
+        let mut board = Board::from_fen(fen_before).unwrap();
+
+        let unmake = board.make_move(mv);
+        assert_eq!(board.to_fen(), fen_after, "Making the move");
+
+        board.unmake_last_move(unmake);
+        assert_eq!(board.to_fen(), fen_before, "Unmaking the move");
+    }
+
+    #[test]
+    fn test_basic_unmake() {
+        test_move_and_unmake(
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            Move {
+                source: Square::B2,
+                destination: Square::B4,
+                promotion: None,
+            },
+            "rnbqkbnr/pppppppp/8/8/1P6/8/P1PPPPPP/RNBQKBNR b KQkq b3 0 1",
+        )
+    }
+
+    #[test]
+    fn test_unmake_en_passant_white() {
+        test_move_and_unmake(
+            // Black just played d7–d5, ep square is d6
+            "rnbqkbnr/ppp1pppp/8/3pP3/8/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 3",
+            Move {
+                source: Square::E5,
+                destination: Square::D6,
+                promotion: None,
+            },
+            "rnbqkbnr/ppp1pppp/3P4/8/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 3",
+        )
+    }
+
+    #[test]
+    fn test_unmake_en_passant_black() {
+        test_move_and_unmake(
+            // White just played e2–e4, ep square is e3
+            "rnbqkbnr/pppppppp/8/8/3Pp3/8/PPP1PPPP/RNBQKBNR b KQkq d3 0 2",
+            Move {
+                source: Square::E4,
+                destination: Square::D3,
+                promotion: None,
+            },
+            "rnbqkbnr/pppppppp/8/8/8/3p4/PPP1PPPP/RNBQKBNR w KQkq - 0 3",
+        )
+    }
+
+    #[test]
+    fn test_unmake_white_short_castle() {
+        test_move_and_unmake(
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQK2R w KQkq - 0 1",
+            Move {
+                source: Square::E1,
+                destination: Square::G1,
+                promotion: None,
+            },
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQ1RK1 b kq - 0 1",
+        )
+    }
+
+    #[test]
+    fn test_unmake_black_short_castle() {
+        test_move_and_unmake(
+            "rnbqk2r/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1",
+            Move {
+                source: Square::E8,
+                destination: Square::G8,
+                promotion: None,
+            },
+            "rnbq1rk1/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQ - 0 2",
+        )
+    }
+
+    #[test]
+    fn test_unmake_white_long_castle() {
+        test_move_and_unmake(
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/R3KBNR w KQkq - 0 1",
+            Move {
+                source: Square::E1,
+                destination: Square::C1,
+                promotion: None,
+            },
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/2KR1BNR b kq - 0 1",
+        )
+    }
+
+    #[test]
+    fn test_unmake_black_long_castle() {
+        test_move_and_unmake(
+            "r3kbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1",
+            Move {
+                source: Square::E8,
+                destination: Square::C8,
+                promotion: None,
+            },
+            "2kr1bnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQ - 0 2",
+        )
+    }
+
+    #[test]
+    fn test_unmake_white_promotion() {
+        test_move_and_unmake(
+            "8/P7/8/8/8/8/8/k6K w - - 0 1",
+            Move {
+                source: Square::A7,
+                destination: Square::A8,
+                promotion: Some(PieceKind::Queen),
+            },
+            "Q7/8/8/8/8/8/8/k6K b - - 0 1",
+        )
+    }
+
+    #[test]
+    fn test_unmake_black_promotion() {
+        test_move_and_unmake(
+            "k6K/8/8/8/8/8/p7/8 b - - 0 1",
+            Move {
+                source: Square::A2,
+                destination: Square::A1,
+                promotion: Some(PieceKind::Queen),
+            },
+            "k6K/8/8/8/8/8/8/q7 w - - 0 2",
+        )
     }
 }
