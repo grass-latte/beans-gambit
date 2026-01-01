bd06bb2b5ceed52d47292fbf76efb04fb3dc35cf
sixthsurge <surgecrafter@gmail.com>
2026-01-01 15:32:08 +0000

    all perft test positions passing

--- chess-lib/src/perft.rs
+++ chess-lib/src/perft.rs
@@ -45,6 +45,8 @@ fn test_perft(label: &str, fen: &str, expected_results: &[u64]) {
     }
 }
 
+// Test positions and Perft results are from https://www.chessprogramming.org/Perft_Results.
+
 #[test]
 fn test_perft_starting_position() {
     test_perft(
@@ -69,5 +71,32 @@ fn test_perft_position_3() {
         "position 3",
         "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1",
         &[14, 191, 2812, 43_238, 674_624],
+    );
+}
+
+#[test]
+fn test_perft_position_4() {
+    test_perft(
+        "position 4",
+        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+        &[6, 264, 9_467, 422_333],
+    );
+}
+
+#[test]
+fn test_perft_position_5() {
+    test_perft(
+        "position 5",
+        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+        &[44, 1_486, 62_379, 2_103_487, 89_941_194],
+    );
+}
+
+#[test]
+fn test_perft_position_6() {
+    test_perft(
+        "position 6",
+        "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+        &[46, 2_079, 89_890, 3_894_594, 164_075_551],
     );
 }
